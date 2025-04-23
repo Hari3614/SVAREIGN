580ec26e04e1097a8ce3abd5ba3c8ee308f55547
Hari prasad R <hariprasad3614@gmail.com>
2025-04-23 11:05:08 +0530

    firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:svareign/firebase_options.dart';
 import 'package:svareign/view/screens/splashscreen/splashscreen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
